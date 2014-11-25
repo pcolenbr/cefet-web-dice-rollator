af512b3671c4de6f7624240ffdc763ffd662648c
pcolenbr <pedro.colen.c.br@gmail.com>
2014-11-25 13:59:53 -0200

    Update principal.js

--- principal.js
+++ principal.js
@@ -1,13 +1,54 @@
-// Faça seu exercício neste arquivo
+// Faï¿½a seu exercï¿½cio neste arquivo
 
 
-// Alguns elementos importantes na página (index.html):
-// #rolar: Botão rolar
-//	 - você deve atribuir um _handler_ de evento a ele para fazer o cálculo da rolagem dos dados
-// #resultado: Elemento cujo conteúdo deve ser uma string com os resultados
-//	 - você deve definir seu conteúdo (.innerHTML) com a string de resultados
+// Alguns elementos importantes na pï¿½gina (index.html):
+// #rolar: Botï¿½o rolar
+//	 - vocï¿½ deve atribuir um _handler_ de evento a ele para fazer o cï¿½lculo da rolagem dos dados
+// #resultado: Elemento cujo conteï¿½do deve ser uma string com os resultados
+//	 - vocï¿½ deve definir seu conteï¿½do (.innerHTML) com a string de resultados
 // #recipienteResultados: "Container" do elemento dos resultados: 
-//	 - você deve torná-lo visível removendo a classe "oculto" dele
+//	 - vocï¿½ deve tornï¿½-lo visï¿½vel removendo a classe "oculto" dele
 // .quantidade: todos os input[type=number] com a quantidade de dados a serem rolados
 // #quantidadeD{4,6,8,10,12,20}: um ID para cada input[type=number] com a quantidade
 
+var rolar = document.getElementById('rolar');
+rolar.addEventListener('click', function(e) {
+    var resultado = document.getElementById('resultado');
+    var recipienteResultado = document.getElementById('recipienteResultados');
+    var finalResult = 0;
+    
+    var quant_d4 = document.getElementById('quantidadeD4').value;
+    var quant_d6 = document.getElementById('quantidadeD6').value;
+    var quant_d8 = document.getElementById('quantidadeD8').value;
+    var quant_d10 = document.getElementById('quantidadeD10').value;
+    var quant_d12 = document.getElementById('quantidadeD12').value;
+    var quant_d20 = document.getElementById('quantidadeD20').value;
+    
+    for(i = 0; i < quant_d4; i++) {
+        finalResult += Math.ceil(Math.random() * 4);
+    }
+    
+    for(i = 0; i < quant_d6; i++) {
+        finalResult += Math.ceil(Math.random() * 6);
+    }
+    
+    for(i = 0; i < quant_d8; i++) {
+        finalResult += Math.ceil(Math.random() * 8);
+    }
+    
+    for(i = 0; i < quant_d10; i++) {
+        finalResult += Math.ceil(Math.random() * 10);
+    }
+    
+    for(i = 0; i < quant_d12; i++) {
+        finalResult += Math.ceil(Math.random() * 12);
+    }
+    
+    for(i = 0; i < quant_d20; i++) {
+        finalResult += Math.ceil(Math.random() * 20);
+    }
+    
+    resultado.innerHTML = finalResult;
+    recipienteResultado.className = "";
+    
+}, false);
